99f2845dd29c80dd887d6be927bdddef320cb9fb
Amaury <amaury.henrottay@gmail.com>
2020-02-18 15:05:18 +0100

    ex challenge ready

---
+++ js/ex-03.js
@@ -0,0 +1,110 @@
+let scene,
+  camera,
+  cloudParticles = [],
+  composer;
+
+function init() {
+  // instance scene & perspective camera
+
+  // ---------------------------------------
+  camera.position.z = 1;
+  camera.rotation.x = 1.16;
+  camera.rotation.y = -0.12;
+  camera.rotation.z = 0.27;
+
+  // add ambient light into the scene with color : 0x555555
+
+  // ---------------------------------------
+
+  let directionalLight = new THREE.DirectionalLight(0xff8c19);
+  directionalLight.position.set(0, 0, 1);
+  scene.add(directionalLight);
+
+  let orangeLight = new THREE.PointLight(0xcc6600, 50, 450, 1.7);
+  orangeLight.position.set(200, 300, 100);
+  scene.add(orangeLight);
+  let redLight = new THREE.PointLight(0xd8547e, 50, 450, 1.7);
+  redLight.position.set(100, 300, 100);
+  scene.add(redLight);
+  let blueLight = new THREE.PointLight(0x3677ac, 50, 450, 1.7);
+  blueLight.position.set(300, 300, 200);
+  scene.add(blueLight);
+
+  // instance renderer
+
+  // ---------------------------------------
+  scene.fog = new THREE.FogExp2(0x03544e, 0.001);
+  renderer.setClearColor(scene.fog.color);
+
+  let loader = new THREE.TextureLoader();
+  loader.load("../assets/img/smoke.png", function(texture) {
+    cloudGeo = new THREE.PlaneBufferGeometry(500, 500);
+    cloudMaterial = new THREE.MeshLambertMaterial({
+      map: texture,
+      transparent: true
+    });
+
+    // put all this instruction in loop with p at 0 to 70
+    /*let cloud = new THREE.Mesh(cloudGeo, cloudMaterial);
+      cloud.position.set(
+        Math.random() * 800 - 400,
+        500,
+        Math.random() * 500 - 500
+      );
+      cloud.rotation.x = 1.16;
+      cloud.rotation.y = -0.12;
+      cloud.rotation.z = Math.random() * 2 * Math.PI;
+      cloud.material.opacity = 0.55;
+      cloudParticles.push(cloud);
+      scene.add(cloud);*/
+    // -------------------------------------------------
+  });
+  loader.load("../assets/img/stars.jpg", function(texture) {
+    const textureEffect = new POSTPROCESSING.TextureEffect({
+      blendFunction: POSTPROCESSING.BlendFunction.COLOR_DODGE,
+      texture: texture
+    });
+    textureEffect.blendMode.opacity.value = 0.2;
+
+    const bloomEffect = new POSTPROCESSING.BloomEffect({
+      blendFunction: POSTPROCESSING.BlendFunction.COLOR_DODGE,
+      kernelSize: POSTPROCESSING.KernelSize.SMALL,
+      useLuminanceFilter: true,
+      luminanceThreshold: 0.3,
+      luminanceSmoothing: 0.75
+    });
+    bloomEffect.blendMode.opacity.value = 1.5;
+
+    let effectPass = new POSTPROCESSING.EffectPass(
+      camera,
+      bloomEffect,
+      textureEffect
+    );
+    effectPass.renderToScreen = true;
+
+    composer = new POSTPROCESSING.EffectComposer(renderer);
+    composer.addPass(new POSTPROCESSING.RenderPass(scene, camera));
+    composer.addPass(effectPass);
+
+    // make responsive your background
+
+    // ---------------------------------------
+    render();
+  });
+}
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+function render() {
+  cloudParticles.forEach(p => {
+    p.rotation.z -= 0.001;
+  });
+  composer.render(0.1);
+  requestAnimationFrame(render);
+}
+
+// call function init :)
+
+// ---------------------
